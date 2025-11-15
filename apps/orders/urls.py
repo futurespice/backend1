9189b4cd92021106a540e052a8dfd42978244c27
futurespice <azatss442@gmail.com>
2025-11-15 13:27:05 +0600

    новые изменения

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,24 +1,24 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import (
-#     PartnerOrderViewSet, StoreOrderViewSet,
-#     OrderHistoryViewSet, OrderReturnViewSet
-# )
-# from rest_framework.routers import DefaultRouter
-# from orders import views
-#
-# router = DefaultRouter()
-# router.register(r'partner-orders', views.PartnerOrderViewSet)
-# router.register(r'store-orders', views.StoreOrderViewSet)
-# router.register(r'order-history', views.OrderHistoryViewSet)
-# router.register(r'order-returns', views.OrderReturnViewSet)
-#
-# router = DefaultRouter()
-# router.register('partner-orders', PartnerOrderViewSet, basename='partner-order')
-# router.register('store-orders', StoreOrderViewSet, basename='store-order')
-# router.register('history', OrderHistoryViewSet, basename='order-history')
-# router.register('returns', OrderReturnViewSet, basename='order-return')
-#
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    PartnerOrderViewSet, StoreOrderViewSet,
+    OrderHistoryViewSet, OrderReturnViewSet
+)
+from rest_framework.routers import DefaultRouter
+from orders import views
+
+router = DefaultRouter()
+router.register(r'partner-orders', views.PartnerOrderViewSet)
+router.register(r'store-orders', views.StoreOrderViewSet)
+router.register(r'order-history', views.OrderHistoryViewSet)
+router.register(r'order-returns', views.OrderReturnViewSet)
+
+router = DefaultRouter()
+router.register('partner-orders', PartnerOrderViewSet, basename='partner-order')
+router.register('store-orders', StoreOrderViewSet, basename='store-order')
+router.register('history', OrderHistoryViewSet, basename='order-history')
+router.register('returns', OrderReturnViewSet, basename='order-return')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- config/settings/base.py
+++ config/settings/base.py
@@ -83,19 +83,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL', 'postgres://baiel_app:12345678@db:5432/baielapp_2'),
-#         conn_max_age=600,
-#         conn_health_checks=True,
-#     )
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgres://baiel_app:12345678@db:5432/baielapp_2'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': dj_database_url.parse(
 #         os.environ.get('DATABASE_URL', 'postgres://baeil_app:12345678@db:5432/baielapp_2')
